b5a5f3a84289974ce77abad61f5538149e4aab8d
Sairam Kaushik <sairamkaushik1@gmail.com>
2022-12-02 18:54:36 +0530

    updated with lottery tickets

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const LoginPage = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-        <Link to="/main"> <button className="text-3xl border-2 border-orange-500 p-4 hover:bg-orange-500 hover:text-white rounded-xl font-popins font-bold">Let's Go</button>
+        <Link to="/main"> <button className="text-3xl border-2 border-orange-500 p-4 hover:bg-orange-500 hover:text-white rounded-xl font-popins font-bold text-gray-600">Let's Go</button>
         </Link>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,19 +2,38 @@ import React from "react";
 
 export const Main = () => {
   return (
-      <div className="w-full h-screen overflow-hidden">
+    <div className="w-full h-screen overflow-hidden mt-36">
+      {/* Header */}
 
-            {/* Header */}
-
-        <div className="flex justify-center">
-          <h1 className="text-3xl font-popins font-bold text-orange-500 mt-8">
-            Welcome to Lucky Draw Context
-          </h1>
-        </div>
-
-        {/* Lottery Cards */}
-
-        
+      <div className="flex justify-center">
+        <h1 className="md:text-3xl text-2xl flex text-center font-popins font-bold text-orange-500 mt-8">
+          Welcome to Lucky Draw Context
+        </h1>
       </div>
+
+      {/* Lottery Cards */}
+      <div className="flex items-center justify-center mt-12 gap-10 flex-wrap">
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-1</button>
+
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-2</button>
+
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-3</button>
+
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-4</button>
+
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-5</button>
+
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-6</button>
+
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-7</button>
+
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-8</button>
+
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-9</button>
+        
+        <button className="border-2 rounded-xl p-4 min-w-[15rem] bg-orange-500 text-white hover:border-orange-500 hover:bg-white hover:text-orange-500 text-2xl">Ticket-10</button>
+
+      </div>
+    </div>
   );
 };
